be4e70950509014e4e4b4898a460a089c844bf01
Julen Leon Larrondo <jleon-la@c4r8s4.42urduliz.com>
2024-07-31 12:18:51 +0200

    Continuing with some built-ins and starting with signals

---
+++ built_ins/export.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   export.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jleon-la <jleon-la@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/31 11:55:38 by jleon-la          #+#    #+#             */
+/*   Updated: 2024/07/31 11:56:14 by jleon-la         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// Dejo este ejemplo por aqi para mirarmelo
+
+bool	check_if_exist(t_shell *env, char *name, char *value)
+{
+	t_my_env	*tmp;
+
+	tmp = env->env_list;
+	while (tmp)
+	{
+		if (check_names(tmp->name, name))
+		{
+			free(tmp->value);
+			tmp->value = value;
+			return (1);
+		}
+		tmp = tmp->next;
+	}
+	return (0);
+}
+
+void	equal_handler(t_shell *shell, char *name, char *value)
+{
+	if (check_if_exist(shell, name, value))
+		return (free(name));
+	add_env(&shell->env_list, ft_envnew(name, value, shell->env_list));
+	if (shell->env_sys_end->next)
+		sort_env(shell->env_sys_end->next);
+}
+
+void	realloc_env(t_shell *shell, char **newvar, bool *error_handler)
+{
+	int		i;
+	char	*split[2];
+	bool	local_error;
+
+	local_error = false;
+	i = 0;
+	while (newvar[++i])
+	{
+		local_error = false;
+		handle_split_env(&split[0], &split[1], newvar[i]);
+		handle_errors_export(split[0], &local_error);
+		if (local_error)
+		{
+			*error_handler = true;
+			free(split[0]);
+			free(split[1]);
+			continue ;
+		}
+		equal_handler(shell, split[0], split[1]);
+	}
+}
+
+int	export(t_shell *shell, char **cmd)
+{
+	bool	error_handler;
+
+	error_handler = false;
+	if (!cmd[1])
+	{
+		print_env(shell->env_list);
+		return (0);
+	}
+	realloc_env(shell, cmd, &error_handler);
+	get_things(shell, true);
+	return ((int)error_handler);
+}
--- built_ins/unset.c
+++ built_ins/unset.c
@@ -6,11 +6,47 @@
 /*   By: jleon-la <jleon-la@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 16:27:46 by jleon-la          #+#    #+#             */
-/*   Updated: 2024/07/08 16:30:35 by jleon-la         ###   ########.fr       */
+/*   Updated: 2024/07/31 11:54:27 by jleon-la         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(int ac, char **av)
+// char    *get_next_line(int fd)                                                                       
+// {                                                                                                                    
+//     static char     buffer[BUFFER_SIZE + 1];                                                                        
+//     char            *line;                                                                                       
+//     ssize_t         bytes_read;                                                                               
+                                                                                                                                                                                                    
+//     line = malloc(sizeof(char) * 1);                                                                                                                                                                                                                                     
+//     line[0] = '\0';                                                                                                                                                   
+//     if (ft_strlen(buffer) >= 1)                                                                                                                                                                     
+//         line = ft_strjoin(line, buffer);                                                                                                                    
+//     while (!ft_strchr(buffer, '\n'))                                                                                                                                                              
+//     {                                                                                                                                                                           
+//         ft_bzero(&buffer, BUFFER_SIZE);                                                                                                                                       
+//         bytes_read = read(fd, buffer, BUFFER_SIZE);                                                                                                                                                                                                                                                                                  
+//         line = ft_strjoin(line, buffer);                                                                                                                                    
+//     }                                                                                                                                                   
+//     if (line[0] == '\0')                                                                                                                                                
+//         return (free(line), NULL);                                                                                                                                                   
+//     return (get_leftover(buffer), line);                                                                                                 
+// }            
+
+
+// int global_status(int new) // Esto es para saber cual ha sido el return del comando anterior
+// {
+//     static int i;
+//     int n;
+
+//     n = i;
+//     if (new >= 0)
+//         i = new;
+//     return (itoa(n));
+// }
+
+// global_status(127)
+// global_status(-1)
+
+int main(int ac, char **av) // Unset se utiliza para borrar variables de entorno
 {
     int i;
 
@@ -19,7 +55,8 @@ int main(int ac, char **av)
     {
         while (i < ac)
         {
-            // Here the progrma must delete specified environment variables
+            // Here the program must delete specified environment variables
+            deleteit(structt, av[i]);
         }
     }
     else
